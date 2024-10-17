65d3a9dc2bef04b4f16919a3cade408773676528
Sonali Bandi <113736487+sonalibandi@users.noreply.github.com>
2024-10-17 17:55:53 -0400

    Update training-script.py

--- training-script.py
+++ training-script.py
@@ -68,7 +68,7 @@ def update_report_file(metrics_dictionary: dict, hyperparameters: dict,
     new_row = dict({'date_time': date_time, 'hyperparameters': json.dumps(hyperparameters), 'commit_hash': commit_hash, 'training_job_name': training_job_name},
                    **metrics_dictionary)
     new_report = pd.DataFrame(new_row, index=[0])
-    reports_df = reports_df.append(new_report)
+    reports_df = reports_df.concat(new_report)
 
     # Upload new reports dataframe
     reports_df.to_csv('./reports.csv', index=False)
